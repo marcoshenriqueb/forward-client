f084d2f3dcc1d1f5a94bc4649c0b7d5418acd684
Marcos Almeida <marcoshenriqueb@gmail.com>
2018-05-01 20:57:27 -0300

    Changes to dashboard

--- src/app/pages/dashboard/dashboard.js
+++ src/app/pages/dashboard/dashboard.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import './dashboard.styl';
+import actions from './../../store/actions';
 
-const Dashboard = ({
-  businessAreas,
-}) => (
-  <div className="dashboard-container d-flex flex-column align-items-center justify-content-center">
-    {
-      businessAreas.data.map(a => (
-        <button className="btn btn-outline-primary btn-lg mb-3" key={a.name}>{a.name}</button>
-      ))
-    }
-    <Link to="/novo-pedido">
-      <button className="btn btn-outline-success btn-lg mb-3">Novo pedido</button>
-    </Link>
-  </div>
-);
+const { logout: logoutAction, fetchOrders: fetchOrdersAction } = actions;
+
+class Dashboard extends Component {
+  componentDidMount() {
+    this.props.fetchOrders();
+  }
+
+  render() {
+    return (
+      <div className="dashboard-container d-flex flex-column">
+        <div className="d-flex justify-content-between align-items-center p-3">
+          <div className="d-flex">
+            {
+              this.props.businessAreas.data.map(a => (
+                <button
+                  className="btn btn-outline-primary btn-lg mr-1"
+                  key={a.name}
+                >
+                  {a.name}
+                </button>
+              ))
+            }
+            <Link to="/novo-pedido">
+              <button className="btn btn-outline-success btn-lg">Novo pedido</button>
+            </Link>
+          </div>
+          <span onClick={this.props.logout}>Sair</span>
+        </div>
+        <div className="d-flex flex-column p-3">
+          <p className="lead">Pedidos</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 Dashboard.propTypes = {
   businessAreas: PropTypes.shape({
@@ -29,11 +51,18 @@ Dashboard.propTypes = {
     ]).isRequired,
     data: PropTypes.array.isRequired,
   }).isRequired,
+  logout: PropTypes.func.isRequired,
+  fetchOrders: PropTypes.func.isRequired,
 };
 
 const DashboardConnector = connect(state => (
   {
     businessAreas: state.businessAreas.businessAreas,
+  }
+), dispatch => (
+  {
+    logout: () => dispatch(logoutAction()),
+    fetchOrders: () => dispatch(fetchOrdersAction()),
   }
 ))(Dashboard);
 
